7c7588ca830ac725c653f7ad7149f4740ce9e36c
CentralMatthew <matvij379@gmail.com>
2021-06-25 16:07:41 +0300

    node homework 2

---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const expressHbs = require('express-handlebars');
+const fs = require('fs/promises');
+const path = require('path');
+
+const app = express();
+
+const staticDir = path.join(__dirname, 'static');
+const usersPath = path.join(__dirname, 'users.json');
+
+app.use(express.static(staticDir));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', '.hbs');
+app.engine('.hbs', expressHbs({
+    defaultLayout: false
+}));
+app.set('views', staticDir);
+
+//-------------------------------------------------------------------------------------
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.get('/registration', (req, res) => {
+    res.render('registration');
+})
+
+app.get('/users', async (req, res) => {
+    const users = await getUsers();
+
+    res.render('users', {users});
+
+})
+app.get('/users/:userId', async (req, res) => {
+    const {userId} = req.params;
+    const users = await getUsers();
+    const user = users[userId];
+
+    res.render('user', {user});
+
+})
+
+//----------------------------------------------------------------------------------------------------------
+
+app.post('/login', async (req, res) => {
+    const {login, password} = req.body
+    const users = await getUsers();
+    const findedUser = users.find(value => value.login === login && value.password === password);
+
+    if (findedUser) {
+        res.redirect(`/users/${users.indexOf(findedUser)}`);
+        return
+    }
+
+    res.render('error-login')
+})
+
+app.post('/registration', async (req, res) => {
+    const {login, password, firstName, lastName, age} = req.body;
+    const users = await getUsers();
+    const newUser = {login, password, firstName, lastName, age, id: users.length + 1};
+
+
+    const userExist = users.find(value => value.login === login);
+    if (userExist) {
+        res.render('error-registration');
+        return
+    }
+
+    users.push(newUser);
+    await fs.writeFile(usersPath, JSON.stringify(users));
+    res.redirect('/login');
+
+})
+
+
+const getUsers = async () => {
+    const users = await fs.readFile(usersPath, 'utf-8');
+    return JSON.parse(users);
+}
+
+
+app.listen(3000, () => {
+    console.log('App listen 3000');
+})
+
